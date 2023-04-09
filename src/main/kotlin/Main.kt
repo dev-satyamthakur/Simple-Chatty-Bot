bee5e979c833a101beb4fabf0ea122a1ddd07987
Satyam Thakur <sat.thakur20@gmail.com>
2023-04-09 19:33:42 +0530

    Add a question to ask

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,64 @@
-import java.util.*
-
 import java.util.Scanner
 
+val scanner = Scanner(System.`in`) // Do not change this line
+
 fun main() {
-    val scanner = Scanner(System.`in`)
+    greet("Aid", "2020") // change it as you need
+    remindName()
+    guessAge()
+    count()
+    test()
+    end()
+}
 
-    println("Hello! My name is Aid.")
-    println("I was created in 2020.")
+fun greet(assistantName: String, birthYear: String) {
+    println("Hello! My name is ${assistantName}.")
+    println("I was created in ${birthYear}.")
     println("Please, remind me your name.")
+}
 
+fun remindName() {
     val name = scanner.nextLine()
+    println("What a great name you have, ${name}!")
+}
 
-    println("What a great name you have, " + name + "!")
+fun guessAge() {
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
-
     val rem3 = scanner.nextInt()
     val rem5 = scanner.nextInt()
     val rem7 = scanner.nextInt()
     val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+    println("Your age is ${age}; that's a good time to start programming!")
+}
 
-    println("Your age is " + age + "; that's a good time to start programming!")
+fun count() {
     println("Now I will prove to you that I can count to any number you want.")
-
-    // read a number and count to it here
-    val n = scanner.nextInt()
-    for (i in 0..n) {
-        println("$i!")
+    val num = scanner.nextInt()
+    for (i in 0..num) {
+        print(i)
+        println("!")
     }
+}
 
-    println("Completed, have a nice day!")
+fun test() {
+    println("Let's test your programming knowledge.")
+    println("What is the color of fruit 'Orange'?")
+    println("1. Red")
+    println("2. Yellow")
+    println("3. Orange")
+    println("4. Green")
+
+    val correctAns = 3
+    while (true) {
+        val input = scanner.nextInt()
+        if (correctAns == input)
+            break
+
+        println("Please, try again.")
+    }
+}
+
+fun end() {
+    println("Congratulations, have a nice day!")
 }
